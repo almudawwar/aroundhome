b3ec1dd976d9b267b1dcfd497a51bb946ee0c3d9
Rodrigo <rodrigopg8@gmail.com>
2021-03-28 17:30:24 +0200

    Add Partner model

---
+++ db/migrate/20210328145416_create_partners.rb
@@ -0,0 +1,14 @@
+class CreatePartners < ActiveRecord::Migration[6.1]
+  def change
+    create_table :partners do |t|
+      t.string :materials, array: true, default: []
+      t.st_point :address, geographic: true, null: false
+      t.float :rating, null: true
+      t.geometry :radius_geo, geographic: true
+      t.integer :operating_radius, null: false
+      t.timestamps
+    end
+
+    add_index :partners, :address, using: :gist
+  end
+end
---
+++ db/schema.rb
@@ -0,0 +1,30 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2021_03_28_145416) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "postgis"
+
+  create_table "partners", force: :cascade do |t|
+    t.string "materials", default: [], array: true
+    t.geography "address", limit: {:srid=>4326, :type=>"st_point", :geographic=>true}, null: false
+    t.float "rating"
+    t.geography "radius_geo", limit: {:srid=>4326, :type=>"geometry", :geographic=>true}
+    t.integer "operating_radius", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["address"], name: "index_partners_on_address", using: :gist
+  end
+
+end
